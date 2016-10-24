7ef1b890306788a5a0e1de06d3e5e9683eb8b8d7
Daniel Engwis <dangwis@umich.edu>
2016-10-24 11:35:48 -0400

    commit

--- Assets/Scripts/SanityLabel.cs
+++ Assets/Scripts/SanityLabel.cs
@@ -4,13 +4,11 @@ using System.Collections;
 public class SanityLabel : MonoBehaviour {
 
     TextMesh label;
-    Renderer textRenderer;
 
     // Use this for initialization
     void Start()
     {
         label = transform.Find("Sanity Label").GetComponent<TextMesh>();
-        textRenderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
--- Assets/Scripts/TorchLabel.cs
+++ Assets/Scripts/TorchLabel.cs
@@ -4,13 +4,11 @@ using System.Collections;
 public class TorchLabel : MonoBehaviour {
 
     TextMesh label;
-    Renderer textRenderer;
 
     // Use this for initialization
     void Start()
     {
         label = transform.Find("Torch Label").GetComponent<TextMesh>();
-        textRenderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
